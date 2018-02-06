f612070648713f157cabe3b84fd392bf86a56887
Dalton Fant <daltonfant16@gmail.com>
2018-02-06 22:46:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,3 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
-
-def batch_badge_creator(array)
-  new_array = []
-  array.each do |person|
-    new_array <<
-  end
-  new_array
-end
