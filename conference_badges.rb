739a3340a42257681bc36ce226ceb7cb4ba16733
Dalton Fant <daltonfant16@gmail.com>
2018-02-06 23:06:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,11 @@ def batch_badge_creator(speakers)
     badge_maker(name)
   end
 end
+
+def assign_rooms(speakers)
+  room = 0
+  speakers.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
